5fafa1d42401e49ba2b3c49ee7fc24d90a514642
chuantuoxiedexiu <314997217@qq.com>
2017-06-20 15:40:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -358,7 +358,6 @@ def get_retweet_list(tweet_ID,s,SUB):
             #         print 'face:',c
             # b=tree[a].xpath('.//div[@class="WB_from S_txt2"]//a[@node-type="feed_list_item_date"]/@title')[0]
             # print 'retweet_time:',b
-            # #retweet like
     return retweet_list
 if __name__ == '__main__':
     #this is the tweet ID
